11a46cdcfe3c0f00f24210d0d20aa69ea89f1009
Atom Software Company <atomco.devteam@gmail.com>
2025-05-16 10:14:18 -0400

    Hotfix on courses

--- src/api/course/routes/course.js
+++ src/api/course/routes/course.js
@@ -1,24 +1,9 @@
 'use strict';
 
-module.exports = {
-  routes: [
-    {
-      method: 'GET',
-      path: '/courses',
-      handler: 'course.find',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'GET',
-      path: '/courses/:id',
-      handler: 'course.findOne',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-  ],
-};
+/**
+ * author router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::course.course');
